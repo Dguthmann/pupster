0e0fb8adb851c9fe84aeff6fd2722a2255d71d82
David Guthmann <drguthmann@cox.net>
2020-11-05 15:42:46 -0600

    working search functionality

--- src/components/pages/Search.js
+++ src/components/pages/Search.js
@@ -5,9 +5,10 @@ import SearchCard from "../SearchCard";
 class Search extends React.Component {
 
   state = {
+    dogBreed:"",
     pictureMap: []
   };
-  
+
 
   apiDogCeo = (mySearch) => {
     API.search(mySearch)
@@ -15,15 +16,45 @@ class Search extends React.Component {
       .catch(err => console.log(err));
   };
 
-  componentDidMount(){
-   this.apiDogCeo("https://dog.ceo/api/breed/hound/images"); 
+  componentDidMount() {
+    this.apiDogCeo("https://dog.ceo/api/breed/hound/images");
   }
-  
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    let value =  event.target.value.toLowerCase();
+    const name = event.target.name;
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    this.apiDogCeo(`https://dog.ceo/api/breed/${this.state.dogBreed}/images`)
+
+    this.setState({
+      dogBreed:""
+    });
+  };
 
   render() {
     return (
-      
+
       <div>
+        <form className="form">
+          <input
+            value={this.state.dogBreed}
+            name="dogBreed"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Pick the Dog Breed"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+
         {this.state.pictureMap.map(mydog => (
           <SearchCard
             mydog={mydog}
