905262204b3e2f69618e3e979af65a8adf0e8930
David Guthmann <drguthmann@cox.net>
2020-11-05 13:29:47 -0600

    adding routes app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Switch>
+        <Route exact path="/" component={About} />
+        <Route exact path="/discover" ></Route>
+        <Route exact path="/search" ></Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
